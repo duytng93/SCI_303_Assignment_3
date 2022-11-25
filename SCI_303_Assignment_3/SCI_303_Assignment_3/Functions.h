d14a3c371e70d4dee9c660ee05d02f86937f9cce
duytng93 <113152725+duytng93@users.noreply.github.com>
2022-11-24 20:27:13 -0600

    Update Functions.h

--- SCI_303_Assignment_3/SCI_303_Assignment_3/Functions.h
+++ SCI_303_Assignment_3/SCI_303_Assignment_3/Functions.h
@@ -10,15 +10,15 @@
 
 template<typename T>
 void move_to_rear(std::queue<T>& testQ) {               //funtion for question 1
-	T item;												//template funtion can't be implemented in cpp file
+	T item;						//template funtion can't be implemented in cpp file
 	item = testQ.front();
 	testQ.push(item);
 	testQ.pop();
 }
 
-bool is_palindrome(std::string str) {					//function for question 2
-	bool result;										//return true if the string is a palindrome
-	if (str.size() == 0 || str.size() == 1)				//return false if the string is not a palindrome
+bool is_palindrome(std::string str) {			//function for question 2
+	bool result;					//return true if the string is a palindrome
+	if (str.size() == 0 || str.size() == 1)		//return false if the string is not a palindrome
 		result = true;
 	else if (str[0] != str[str.length() - 1])
 		result = false;
@@ -28,7 +28,7 @@ bool is_palindrome(std::string str) {					//function for question 2
 }
 
 void display_mapData(std::map<std::string, std::string> stateDataMap) {				//function for question 5			
-	std::map <std::string, std::string>::iterator itr;								//method that outputs the data stored in stateDataMap
+	std::map <std::string, std::string>::iterator itr;					//method that outputs the data stored in stateDataMap
 	std::cout << "State Map Data: " << std::endl << std::endl;
 	std::cout << "  " << std::left << std::setw(15) << "State" << "Capital" << std::endl;
 	std::cout << "-------------------------------" << std::endl;
@@ -39,11 +39,11 @@ void display_mapData(std::map<std::string, std::string> stateDataMap) {				//fun
 }
 
 void lookupCapital(std::map<std::string, std::string> stateDataMap) {				//function for question 5	
-	std::string stateName;															//method that outputs the capitalName using the stateName 
-	std::cout << "Enter a State name: ";											//which will be entered by the user. 
-	getline(std::cin, stateName);							//get user input
+	std::string stateName;									//method that outputs the capitalName using the stateName 
+	std::cout << "Enter a State name: ";							//which will be entered by the user. 
+	getline(std::cin, stateName);					//get user input
 
-	stateName[0] = toupper(stateName[0]);					//format user input to have the first letter of every word capitalized
+	stateName[0] = toupper(stateName[0]);				//format user input to have the first letter of every word capitalized
 	for (int i = 1; i < stateName.length(); ++i) {
 		stateName[i] = tolower(stateName[i]);
 		if (stateName[i] == ' ') {
@@ -62,7 +62,7 @@ void lookupCapital(std::map<std::string, std::string> stateDataMap) {				//funct
 			std::cout << itr->second << " is the capital of " << itr->first;
 		}
 	}
-	if (is_found == false)														//output a message if no match found
+	if (is_found == false)								//output a message if no match found
 		std::cout << stateName << " is not found in the State Data Map";
 }
 
