78ba290b4820f8369d70376b84e6c881ff0d68a4
duytng93 <113152725+duytng93@users.noreply.github.com>
2022-12-01 21:07:41 -0600

    Update Main.cpp

--- SCI_303_Assignment_3/SCI_303_Assignment_3/Main.cpp
+++ SCI_303_Assignment_3/SCI_303_Assignment_3/Main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
 	
-	queue<int> testQ, tempQ;					//question 1
+	queue<int> testQ, tempQ;				//question 1
 	cout << "Original queue: ";					
 	for (int i = 0; i < 6; i++) {				
 		testQ.push(i);					// insert 0,1,2,3,4,5 to the queue
@@ -12,20 +12,20 @@ int main() {
 	}
 	cout << endl;
 
-	move_to_rear(testQ);				//function to move the first element to the end of the queue
+	move_to_rear(testQ);					//function to move the first element to the end of the queue
 	tempQ = testQ;						// temporary queue is use to output queue elements
 	cout << "Modified queue: ";
 	size_t size = tempQ.size();
-	for (int i = 0; i < size ; i++) {	//loop to display queue elements
-		cout << tempQ.front() << " ";	//output the first element 
+	for (int i = 0; i < size ; i++) {			//loop to display queue elements
+		cout << tempQ.front() << " ";			//output the first element 
 		tempQ.pop();					//then remove the first element in order to output next element in the next loop
 	}
 	cout << endl << endl<<"==========================================="<<endl<<endl;
 
 	//////////////////////////////////////////////////////////////////////////////////
 
-	bool result;																	//question 2
-	result = is_palindrome("abcd 123");												//funtion is_palindrome take a string as a parementer
+	bool result;										//question 2
+	result = is_palindrome("abcd 123");							//funtion is_palindrome take a string as a parementer
 	cout << "\"abcd 123\" is a palindrome : " << boolalpha << result <<endl;
 	result = is_palindrome("");
 	cout << "an empty string is a palindrome: " << boolalpha << result << endl;
@@ -37,22 +37,22 @@ int main() {
 
 	//////////////////////////////////////////////////////////////////////////////////
 
-	map<string, string> stateDataMap;							//question 5
+	map<string, string> stateDataMap;					//question 5
 
 	stateDataMap.insert({ "Nebraska","Lincoln" });				//statements that add the following pairs to stateDataMap:   
 	stateDataMap.insert({ "New York","Albany" });				//(Nebraska, Lincoln), (New York, Albany), (Ohio, Columbus),
-	stateDataMap.insert({ "Ohio","Columbus" });					//(California, Sacramento), (Massachusetts, Boston), and (Texas, Austin).
+	stateDataMap.insert({ "Ohio","Columbus" });				//(California, Sacramento), (Massachusetts, Boston), and (Texas, Austin).
 	stateDataMap.insert({ "California","Sacramento" });
 	stateDataMap.insert({ "Massachusetts","Boston" });
 	stateDataMap.insert({ "Texas","Austin" });
 
-	display_mapData(stateDataMap);								//method that outputs the data stored in stateDataMap
+	display_mapData(stateDataMap);							//method that outputs the data stored in stateDataMap
 
 	stateDataMap["California"] = "Los Angeles";					//statement that changes the capital of California to Los Angeles 
 	cout << endl <<"Capital of California is changed to Los Angeles!" << endl <<endl;
 		
-	lookupCapital(stateDataMap);								//method that outputs the capitalName using the stateName  
-	cout << endl<<endl;											//which will be entered by the user.
+	lookupCapital(stateDataMap);							//method that outputs the capitalName using the stateName  
+	cout << endl<<endl;								//which will be entered by the user.
 	lookupCapital(stateDataMap);
 	
 	cout << endl<<endl;
